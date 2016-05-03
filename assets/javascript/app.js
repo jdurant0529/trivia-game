55d5c827a3d5bd511dfc25936f29ae00a69477a0
jdurant0529 <james.durant@gmail.com>
2016-05-03 13:29:36 -0400

    new buttons and image for questions.  Rolling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,13 +9,16 @@ var game = {
 	time:15,
 	questionNum : 0,
 
-	 trivia : [
-	{id: 1, Question: 'Question 1?', Option1: 'Option 1', Option2: 'Option 2', Option3: 'Option 3', Option4: 'Option 4', Answer: 'Option 2'},
-	{id: 2, Question: 'Question 1?', Option1: 'Option 1', Option2: 'Option 2', Option3: 'Option 3', Option4: 'Option 4', Answer: 'Option 1'},
-	{id: 3, Question: 'Question 1?', Option1: 'Option 1', Option2: 'Option 2', Option3: 'Option 3', Option4: 'Option 4', Answer: 'Option 2'},
-	{id: 4, Question: 'Question 1?', Option1: 'Option 1', Option2: 'Option 2', Option3: 'Option 3', Option4: 'Option 4', Answer: 'Option 4'},
-	{id: 5, Question: 'Question 1?', Option1: 'Option 1', Option2: 'Option 2', Option3: 'Option 3', Option4: 'Option 4', Answer: 'Option 3'},
-	],
+
+	trivia : [
+    	{id: 1, Question: 'Question 1?', Options: ['option1', 'Arizona Cardinals', 'option3', 'option4' ], answer: 'Arizona Cardinals', image: "<img src='assets/images/Arizona_Cardinals.jpg'>"},
+    	{id: 2, Question: 'Question 2?', Options: ['option1', 'option2', 'option3', 'option4' ], answer: 'option2'},
+    	{id: 3, Question: 'Question 3?', Options: ['option1', 'option2', 'option3', 'option4' ], answer: 'option2'},
+    	{id: 4, Question: 'Question 4?', Options: ['option1', 'option2', 'option3', 'option4' ], answer: 'option2'},
+    	{id: 5, Question: 'Question 5?', Options: ['option1', 'option2', 'option3', 'option4' ], answer: 'option2'},
+    	{id: 6, Question: 'Question 6?', Options: ['option1', 'option2', 'option3', 'option4' ], answer: 'option2'},
+    ],
+	
 
 	begin: function(){
 	    game.time--;
@@ -45,7 +48,6 @@ var game = {
   		$('#start').hide();
   		console.log(game.questionNum);
   		var question = game.nextQuestion(game.questionNum);
-  		console.log(question);
   		$('#questionSection').html(question);
   		game.questionNum++;
   		$('#responseSection').empty();
@@ -67,34 +69,36 @@ var game = {
 	    return minutes + ":" + seconds;
 	},
 	nextQuestion: function(q){
-		console.log('game.trivia[q].question = ' + game.trivia[q].Question);
+		console.log('game.trivia[q].Question = ' + game.trivia[q].Question);
 		console.log('q = ' + q);
-		var displayHTML = '<div class ="row"> <div class = col-xl-12>'+ this.trivia[q].Question + '</div></div>' 
-		// $('questionSection').append(displayHTML);
-		// for (var i = 0; i < game.trivia.length; i++) {    
-		// 	var currbutton = 'option' + (i+1);
-		// 	console.log(game.trivia[i].currbutton);
-		//     var b = $('<button>');
-		//      b.addClass('answer1 ' + game.trivia[i].id);
-		//      b.attr('data-name', game.trivia[i].name);
-		//      b.html(game.trivia[i].currbutton);
-		//      $('#questionSection').append(b);
-		// }
-		
-			displayHTML = displayHTML + '<button id = button>' + this.trivia[q].Option1 + '</button>';
-			displayHTML = displayHTML + '<button id = button>' + this.trivia[q].Option2 + '</button>';
-			displayHTML = displayHTML + '<button id = button>' + this.trivia[q].Option3 + '</button>';
-			displayHTML = displayHTML + '<button id = button>' + this.trivia[q].Option4 + '</button>';
+		$('#questionSection').html('<h1>Name this team!</h1>' + game.trivia[q].image + '<div>');
 
-		return displayHTML;
+			for (var i = 0; i < game.trivia[q].Options.length; i++) {    
+			    var b = $('<button>');
+			    b.addClass('option ' + game.trivia[q].id);
+			    b.attr('data-Option1', game.trivia[q].Options[i]); 
+			    // b.attr('data-HP', characters[i].HP);
+			    // b.attr('data-AP', characters[i].AP);
+			    // b.attr('data-CP', characters[i].CP);
+			    b.attr('data-num', i);
+			    b.text(game.trivia[q].Options[i]);
+			    b.attr('id',game.trivia[q].id);
+			    //b.html(game.trivia[q].image);
+
+
+			    $("#questionSection").append(b); 
+			    //alert('watch this');
+			}
+		$('#questionSection').append('</div>')
+		//return displayHTML;
 	},
 
 	nextresult: function (success){
 		console.log();
-	}
+	},
 
-	// $('.button').on('click',function(){
-	// 	console.log(this.Answer)
+	// $('#button').on('click',function(){
+	// 	console.log('I am here');
 	// });
 };
 
