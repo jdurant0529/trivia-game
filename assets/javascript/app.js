e3cf5cdeccdd58b55a5afe9eaef5b364ba57332a
jdurant0529 <james.durant@gmail.com>
2016-05-03 21:07:35 -0400

    reset working correctly.  more questions added.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,15 +8,17 @@ var game = {
 
 	time:15,
 	questionNum : 0,
+	correctCount: 0,
+	responseTime: 3,
 
 
 	trivia : [
     	{id: 1, image: "<img src='assets/images/Arizona_Cardinals.jpg'>", Options: ['Atlanta Falcons', 'Arizona Cardinals', 'Seattle Seahawks', 'Toronto Blue-Jays' ], answer: 'Arizona Cardinals'},
     	{id: 2, image: "<img src='assets/images/New_York_Giants.jpg'>", Options: ['New York Giants', 'New York Jets', 'New York Yankees', 'New York Mets' ], answer: 'New York Giants'},
-    	{id: 3, Question: 'Question 3?', Options: ['option1', 'option2', 'option3', 'option4' ], answer: 'option2'},
-    	{id: 4, Question: 'Question 4?', Options: ['option1', 'option2', 'option3', 'option4' ], answer: 'option2'},
-    	{id: 5, Question: 'Question 5?', Options: ['option1', 'option2', 'option3', 'option4' ], answer: 'option2'},
-    	{id: 6, Question: 'Question 6?', Options: ['option1', 'option2', 'option3', 'option4' ], answer: 'option2'},
+    	{id: 3, image: "<img src='assets/images/New_Orleans_Saints.jpg'>", Options: ['option1', 'option2', 'New Orleans Saints', 'option4' ], answer: 'New Orleans Saints'},
+    	{id: 4, image: "<img src='assets/images/Philadelphia_Eagles.jpg'>", Options: ['Philadelphia Eagles', 'option2', 'option3', 'option4' ], answer: 'Philadelphia Eagles'},
+    	{id: 5, image: "<img src='assets/images/Houston_Texans.jpg'>", Options: ['option1', 'option2', 'Houston Texans', 'option4' ], answer: 'Houston Texans'},
+    	{id: 6, image: "<img src='assets/images/Green_Bay_Packers.jpg'>", Options: ['option1', 'option2', 'option3', 'Green Bay Packers' ], answer: 'Green Bay Packers'},
     ],
 	
 
@@ -24,19 +26,22 @@ var game = {
 	    game.time--;
 		var converted = game.timeConverter(game.time);
     	$('#display').html(converted);
-    	// if (game.time == 0) {
-	    // 	game.stop();
-	    // 	$('#reset').show();
-	    // 	$('#questionSection').empty();
-	    // 	$('#responseSection').empty();
-	    //  $('#resultSection').show();
-	    // }
+    	if (game.time == 0) {
+	    	game.showResults();
+
+	    	
+	    }
   	},
   	reset: function(){
 	    game.time = 15;
+	    game.questionNum = 0;
+	    game.correctCount = 0;
 	    converted = game.timeConverter(game.time);
     	$('#display').html(converted);
     	$('#reset').hide();
+    	$('#resultSection').empty();
+    	$('#resultSection').hide();
+    	$('#questionSection').show();
     	$('#start').show();
   	},
   	stop: function(){
@@ -44,12 +49,13 @@ var game = {
   	},
   	
   	start: function(){
+  		$('#questionSection').show();
   		counter = setInterval(game.begin, 1000);
   		$('#start').hide();
   		console.log(game.questionNum);
   		var question = game.nextQuestion(game.questionNum);
   		$('#questionSection').html(question);
-  		game.questionNum++;
+
   		$('#responseSection').empty();
   		$('#resultSection').empty();
  	},
@@ -88,13 +94,40 @@ var game = {
 			console.log($(this).data('answer'));
 			var check = ($(this).data('option') == $(this).data('answer'))
 			console.log(check);
-			
+			game.nextResponse(check);
 			
 		});		//end of onclick for answer option buttons
 	},          // end of nextQuestion -- creates and adds all things pertaining to question into question Section.
 
 	nextResponse: function(c){
 		console.log(c);
+		$('#questionSection').empty();
+		if (c == true) {
+			var responseHTML = "Thats correct!!"
+			game.correctCount++;
+			$('#responseSection').html(responseHTML);
+		} else {
+			var responseHTML = "Sorry, that was not correct."
+			$('#responseSection').html(responseHTML);
+		}
+		game.questionNum++
+		console.log(game.questionNum);
+		question = game.nextQuestion(game.questionNum);
+  		$('#questionSection').html(question);
+  		$('#responseSection').empty();
+  		$('#resultSection').empty();
+	},
+
+
+	showResults: function(){
+		game.stop();
+    	$('#reset').show();
+    	$('#questionSection').empty();
+    	$('#responseSection').empty();
+     	$('#resultSection').show();
+     	var resultsHTML = '<h1>There were ' + game.questionNum + ' total questions asked.</h1>' + 
+     		'<h1>You answered ' + game.correctCount	+ 'correctly.</h1>'
+		$('#resultSection').html(resultsHTML);
 	}
 
 };
